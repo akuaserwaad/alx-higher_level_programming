396669053971b2ef47ae9e7cb0f6ec67d9d123f5
akuaserwaad <akuaserwaa93@gmail.com>
2024-01-10 13:34:35 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix = []:
+        return matrix
+    new_matrix = len(matrix)
+    for i in matrix:
+        new_matrix = matrix.append(map(lambda x: x**2, i))
+    return new_matrix
